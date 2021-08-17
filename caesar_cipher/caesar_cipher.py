4b76aaecde869d6bf6f9724f3f578baff58eb6c7
abdu-zeyad <abd.d@live.com>
2021-08-17 17:19:25 +0300

    finish the lab

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,47 +1,18 @@
 import re
 import nltk
 from nltk.corpus import words, names
-
 nltk.download('words', quiet=True)
 nltk.download('names', quiet=True)
-
 word_list = words.words()
 name_list = names.words()
 
 
-candidates = [
-    "a dark and stormy night",
-    "n qnex naq fgbezl avtug",
-    "j mjat jwm bcxavh wrpqc",
-    "call me Ishmael",
-    "Billy Pilgrim has become unstuck in time",
-    "All happy families are alike; each unhappy family is unhappy in its own way.",
-    '"Where\'s Papa going with that ax?" said Fern to her mother as they were setting the table for breakfast.',
-    "Off the hizzle fo shizzle",
-]
-
-def count_words(text):
-    candidate_words = text.split()
-
-    word_count = 0
-
-    for candidate in candidate_words:
-        word = re.sub(r'[^A-Za-z]+','', candidate)
-        if word.lower() in word_list or word in name_list:
-            # print("english word", word)
-            word_count += 1
-        else:
-            pass
-            # print('not english word or name', word)
-
-    return word_count
-
 def encrypt(plain_text, shifted):
-# # In Python, the ord() function accepts a string of unit length as an argument 
-# and returns the Unicode equivalence of the passed argument. 
+
 
     encrypted = ""
-
+    # # In Python, the ord() function accepts a string of unit length as an argument 
+    # and returns the Unicode equivalence of the passed argument. 
     for c in plain_text:
 
         if c.isupper():  
@@ -92,15 +63,19 @@ def decrypt(ciphertext, shifted):
     return decrypted
 
 
-x = encrypt('Hello15', 3)
-print(x)
-y = decrypt(x, 3)
-print(y)
+def crack(ciphertext):
+    for i in range(26):
+        word_decrypted =  decrypt(ciphertext,i)
+        word = re.sub(r'[^A-Za-z]+','', word_decrypted)
+        if word.lower() in word_list or word in name_list:
+            print(word_decrypted)
+
+encrypted_word = encrypt('Hello!3', 7)
+decrypted_word= decrypt(encrypted_word, 7)
+decrypted_word_without_thekey = crack(encrypted_word)
 
 
 
-# for phrase in candidates:
-#     word_count = count_words(phrase)
-#     percentage = int(word_count / len(phrase.split()) * 100)
-#     if percentage > 50:
-#         print(phrase, percentage
+
+
+
